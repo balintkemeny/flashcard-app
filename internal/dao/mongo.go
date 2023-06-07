420cdc76e77f8724fe34339ce403187e602b2691
Balint Kemeny <52007126+balintkemeny@users.noreply.github.com>
2023-06-07 19:57:00 +0200

    Fix inconsistent receiver naming

--- internal/dao/mongo.go
+++ internal/dao/mongo.go
@@ -34,15 +34,15 @@ func (m *Mongo) InsertOne(collectionName string, v interface{}) error {
 	return nil
 }
 
-func (cm *Mongo) GetAll(collectionName string, dest interface{}) error {
-	coll := cm.DB.Collection(collectionName)
+func (m *Mongo) GetAll(collectionName string, dest interface{}) error {
+	coll := m.DB.Collection(collectionName)
 
-	cursor, err := coll.Find(cm.Ctx, bson.M{})
+	cursor, err := coll.Find(m.Ctx, bson.M{})
 	if err != nil {
 		return fmt.Errorf("%w %s: %w", ErrGetDocuments, collectionName, err)
 	}
 
-	if err := cursor.All(cm.Ctx, dest); err != nil {
+	if err := cursor.All(m.Ctx, dest); err != nil {
 		return errors.Join(ErrUnmarshal, err)
 	}
 
